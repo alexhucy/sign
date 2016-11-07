bdc2b10c413bcd4e8e42f875412124f76d5f5003
alexhucy <alexhucy@126.com>
2016-11-07 19:45:28 +0800

    submit code

--- config/config.dev.js
+++ config/config.dev.js
@@ -3,36 +3,15 @@
  */
 
 module.exports = {
-	// appid: 'wx77025818d05fc144',
-	// appsecret: 'df9d2adcea9a29136c31ef0b3b496551',
-	// mchId: '1370367102',
-	// token: 'Taidii',
-	// partnerKey: 'fa7f290a56b7d6edaed2b74c261d0732',
-	// notifyUrl: 'http://back.test.baoming.xingaokaowang.cn/order/wxpay/callback/',
-	// app_key: 'node_app',
-	// app_secret:'1234',
-	// domain: 'http://test.baoming.xingaokaowang.cn',
-	// logic: {
-	// 	host:'back.test.baoming.xingaokaowang.cn',
-	// 	port:'80',
-	// 	createUser:'/api/get-token',
-	// 	sign: '/signup/signup/',
-	// 	orderList: '/signup/signup/list/',
-	// 	orderInfo: '/signup/signup/{id}/order/',
-	// 	updateOrder: '/signup/signup/{id}',
-	// 	payInfo: '/signup/signup/{id}/pay/',
-	// 	commit: '/signup/signup/{id}/commit/'
-	// },
-	// session_secret:'haojiaoyubaoming'
-	appid: 'wxc3378269c5fd45a7',
-	appsecret: '5019a6f743905ac296df161fba2a7c4a',
+	appid: 'wx77025818d05fc144',
+	appsecret: 'df9d2adcea9a29136c31ef0b3b496551',
 	mchId: '1370367102',
 	token: 'Taidii',
 	partnerKey: 'fa7f290a56b7d6edaed2b74c261d0732',
-	notifyUrl: 'http://tao.s1.natapp.cc',
+	notifyUrl: 'http://back.test.baoming.xingaokaowang.cn/order/wxpay/callback/',
 	app_key: 'node_app',
 	app_secret:'1234',
-	domain: 'http://edu.ngrok.chainz.net',
+	domain: 'http://test.baoming.xingaokaowang.cn',
 	logic: {
 		host:'back.test.baoming.xingaokaowang.cn',
 		port:'80',
@@ -45,4 +24,25 @@ module.exports = {
 		commit: '/signup/signup/{id}/commit/'
 	},
 	session_secret:'haojiaoyubaoming'
+	// appid: 'wxc3378269c5fd45a7',
+	// appsecret: '5019a6f743905ac296df161fba2a7c4a',
+	// mchId: '1370367102',
+	// token: 'Taidii',
+	// partnerKey: 'fa7f290a56b7d6edaed2b74c261d0732',
+	// notifyUrl: 'http://tao.s1.natapp.cc',
+	// app_key: 'node_app',
+	// app_secret:'1234',
+	// domain: 'http://edu.ngrok.chainz.net',
+	// logic: {
+	// 	host:'back.test.baoming.xingaokaowang.cn',
+	// 	port:'80',
+	// 	createUser:'/api/get-token',
+	// 	sign: '/signup/signup/',
+	// 	orderList: '/signup/signup/list/',
+	// 	orderInfo: '/signup/signup/{id}/order/',
+	// 	updateOrder: '/signup/signup/{id}',
+	// 	payInfo: '/signup/signup/{id}/pay/',
+	// 	commit: '/signup/signup/{id}/commit/'
+	// },
+	// session_secret:'haojiaoyubaoming'
 }
